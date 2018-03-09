e05b49451021c8793fcdc141d2196b89f313d0e8
valeraf23 <valeraf23@gmail.com>
2018-03-09 16:47:30 +0200

    work regarding comments

--- LendOrder.sol
+++ LendOrder.sol
@@ -1,8 +1,11 @@
 pragma solidity ^0.4.20;
 
 import "./ownable.sol";
+import "./SafeMath.sol";
 
 contract OffChain is Ownable{
+ 
+ using SafeMath for uint256;
  
     event NewOrder(uint id, uint _amount);
     event NewUser(string _userName);
@@ -22,7 +25,7 @@ contract OffChain is Ownable{
     
     function lendMoney(uint _amount) public onlyKnownUser() moreThanZero(_amount) {
         uint id = orders.push(Order(userNameByAddr[msg.sender], _amount, uint32(now),"lend")) - 1;
-        balanceByAddr[msg.sender] = balanceByAddr[msg.sender] + _amount;
+        balanceByAddr[msg.sender] = balanceByAddr[msg.sender].add(_amount);
         NewOrder(id, _amount);
     }
     
@@ -30,7 +33,7 @@ contract OffChain is Ownable{
         require(_debtorAddress > 0x0);
         require(balanceByAddr[_debtorAddress] >= _amount);
         uint id = orders.push(Order(userNameByAddr[_debtorAddress],_amount, uint32(now),"repay")) - 1;
-        balanceByAddr[_debtorAddress] = balanceByAddr[_debtorAddress] - _amount;
+        balanceByAddr[_debtorAddress] = balanceByAddr[_debtorAddress].sub(_amount);
         NewOrder(id, _amount);
     }
 	
